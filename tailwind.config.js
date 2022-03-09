603c798d1aa3bebb0077e54c83ab2f0c5d6e176a
Janet Schindler <nic07_schindler@web.de>
2022-03-09 14:51:26 +0100

    changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     './public/*.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+        backgroundImage: {
+          'hero-pattern': "url('https://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80')"
+      
+        }
+    
+    },
     screens: {
       'sm': '640px',
       // => @media (min-width: 640px) { ... }
