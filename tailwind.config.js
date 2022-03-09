21b6aaed5312eb81d53a37d7667c7090bbb28b90
Janet Schindler <nic07_schindler@web.de>
2022-03-09 16:57:07 +0100

    delete wrong Data

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,7 @@ module.exports = {
   theme: {
     extend: {
       
-        backgroundImage: {
-          'hero-pattern': "url('https://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80')"
-      
-        }
+       
     
     },
     screens: {
